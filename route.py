ded55e99c2f2f91ae6b50b43137333d13f302945
Franklin Yuan <franklin.yuan.chch@gmail.com>
2024-06-23 20:29:06 +1200

    Deleted extra files, added time function

--- route.py
+++ route.py
@@ -1,24 +1,20 @@
 from flask import Flask, render_template
-import sqlite3
+# import sqlite3
 
 
 app = Flask(__name__)
 
 @app.route("/")
-def calculator():
-    return render_template("layout.html")
+def home():
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
     return(":(")
 
-@app.route("/results/<int:id>")
-def result(id):
-    conn = sqlite3.connect("results.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM sessions WHERE =?",(id,))
-    pizza = cur.fetchone()
-    return render_template("pizza.html", pizza=pizza)
+@app.route("/timer")
+def result():
+    return render_template("timer.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
