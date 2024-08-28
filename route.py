63cdb8aa09964adf86536e4e4a85da61dc884473
Franklin Yuan <franklin.yuan.chch@gmail.com>
2024-08-29 09:55:45 +1200

    + Added 500 Server error handling - Changed displayed text for Ao5 from "Calculating..." to "#N/A" when there is less than 5 solves in a session.

--- route.py
+++ route.py
@@ -382,5 +382,10 @@ def solve_stats(solve_id):
 def page_not_found(e):
     return render_template('404.html'), 404
 
+# Route for handling server error
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
